8378634c9726c355251cb3f279aedbc09ed66238
Tanktastic6400 <159968548+Tanktastic6400@users.noreply.github.com>
2024-06-27 14:05:46 -0500

    uploading studio for loops

--- loops/studio/solution.js
+++ loops/studio/solution.js
@@ -12,17 +12,21 @@ let desserts = ['apple', 'banana', 'more kale', 'ice cream', 'chocolate', 'kiwi'
 function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
   let pantry = [protein, grains, veggies, beverages, desserts];
   let meals = [];
+  let items = [];
   
   /// Part A #2: Write a ``for`` loop inside this function
   /// Code your solution for part A #2 below this comment (and above the return statement) ... ///
   
   for(let i = 0; i < numMeals; i++){
-
+    //reset items
+    items = [];
+    
     for(let j = 0; j < pantry.length; j++){      
-      meals.push(pantry[j][i]);
+      items.push(pantry[j][i]);
       
     }//end of nested for loop
-    //meals.push(tmpArr);
+    
+    meals.push(items.concat());
   }//end of first for loop
   
 
@@ -32,7 +36,17 @@ function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
 
 function askForNumber() {
   numMeals = input.question("How many meals would you like to make?");
-  
+  let validInput = false;
+  let num;
+
+ // input.question("How many meals would you like to create? ");
+  while(!validInput){
+    if(numMeals > 0 && numMeals <= 6){
+      validInput = true;
+    }else{
+      input.question("Please input something between 1 and 6 meals: ");
+    }
+  }
   /// CODE YOUR SOLUTION TO PART B here ///
 
   return numMeals;
@@ -41,9 +55,13 @@ function askForNumber() {
 
 function generatePassword(string1, string2) {
   let code = '';
-
+  let tmpArr = [];
+  for(let i = 0; i < string1.length + string2.length; i++){
+    tmpArr.push(string1[i]);
+    tmpArr.push(string2[i]);
+  }
   /// Code your Bonus Mission Solution here ///
-
+  code = tmpArr.join("");
   return code;
 }
 
@@ -54,7 +72,7 @@ function runProgram() {
   /// Change the final input variable (aka numMeals) here to ensure your solution makes the right number of meals ///
   /// We've started with the number 2 for now. Does your solution still work if you change this value? ///
   
-   let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
+   let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 4);
    console.log(meals)
   
 
@@ -62,16 +80,16 @@ function runProgram() {
   /// UNCOMMENT the next two lines to test your ``askForNumber`` solution ///
   /// Tip - don't test this part until you're happy with your solution to part A #2 ///
   
-  // let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
-  // console.log(mealsForX);
+   let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
+   console.log(mealsForX);
 
     /// TEST PART C HERE ///
   /// UNCOMMENT the remaining commented lines and change the password1 and password2 strings to ensure your code is doing its job ///
 
-  // let password1 = '';
-  // let password2 = '';
-  // console.log("Time to run the password generator so we can update the menu tomorrow.")
-  // console.log(`The new password is: ${generatePassword(password1, password2)}`);
+   let password1 = 'LoOt';
+   let password2 = 'oku!';
+   console.log("Time to run the password generator so we can update the menu tomorrow.")
+   console.log(`The new password is: ${generatePassword(password1, password2)}`);
 }
 
 module.exports = {
